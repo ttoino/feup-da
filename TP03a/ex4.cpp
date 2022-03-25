20a206dd41349340e964610e9ef6a38d19ee2b96
toino <joaoapereira21@hotmail.com>
2022-03-25 17:55:52 +0000

    Add solutions for exercises 1, 4 and 5 of TP03a

--- TP03a/ex4.cpp
+++ TP03a/ex4.cpp
@@ -1,23 +1,46 @@
+#include <cstdint>
+#include <memory>
+#include <sstream>
+
 #include "exercises.h"
 
 std::string calcSum(int sequence[], unsigned long n) {
-    // TODO
-    return "";
+    auto s = std::make_unique<int[]>(n);
+    std::stringstream st{};
+
+    for (int m = 0; m < n; ++m) {
+        int mins = INT_MAX;
+        int mini = INT_MAX;
+
+        for (int i = 0; i < n - m; ++i) {
+            s[i] += sequence[i + m];
+
+            if (s[i] < mins) {
+                mins = s[i];
+                mini = i;
+            }
+        }
+
+        st << mins << ',' << mini << ';';
+    }
+
+    return st.str();
 }
 
 void testPerformanceCalcSum() {
-    //TODO
+    // TODO
 }
 
 /// TESTS ///
 #include <gtest/gtest.h>
 
 TEST(TP4_Ex4, CalcSumArrayTest) {
-    int sequence[5] = {4,7,2,8,1};
-    int sequence2[9] = {6,1,10,3,2,6,7,2,4};
+    int sequence[5] = {4, 7, 2, 8, 1};
+    int sequence2[9] = {6, 1, 10, 3, 2, 6, 7, 2, 4};
 
-    EXPECT_EQ("1,4;9,1;11,2;18,1;22,0;",calcSum(sequence, 5));
-    EXPECT_EQ("1,1;5,3;11,3;16,1;20,3;24,3;31,1;35,1;41,0;",calcSum(sequence2, 9));
-	
-	testPerformanceCalcSum();
+    EXPECT_EQ("1,4;9,1;11,2;18,1;22,0;", calcSum(sequence, 5));
+    EXPECT_EQ("1,1;5,3;11,3;16,1;20,3;24,3;31,1;35,1;41,0;",
+              calcSum(sequence2, 9));
+
+    testPerformanceCalcSum();
 }
--- TP03a/ex5.cpp
+++ TP03a/ex5.cpp
@@ -1,44 +1,64 @@
+#include <memory>
+
 #include "exercises.h"
 
 unsigned long s_recursive(unsigned int n, unsigned int k) {
-    // TODO
-    return 0;
+    if (k == 1 || k == n)
+        return 1;
+
+    return s_recursive(n - 1, k - 1) + k * s_recursive(n - 1, k);
 }
 
-unsigned long b_recursive(unsigned int n) {
-    // TODO
-    return 0;
+unsigned long _b_recursive(unsigned int n, unsigned int k) {
+    if (k > n)
+        return 0;
+
+    return s_recursive(n, k) + _b_recursive(n, k + 1);
 }
+
+unsigned long b_recursive(unsigned int n) { return _b_recursive(n, 1); }
 
 unsigned long s_dynamic(unsigned int n, unsigned int k) {
-    // TODO
-    return 0;
+    int maxj = n - k;
+    auto c = std::make_unique<unsigned long[]>(maxj + 1);
+
+    for (int j = 0; j <= maxj; ++j)
+        c[j] = 1;
+
+    for (int i = 2; i <= k; ++i)
+        for (int j = 1; j <= maxj; ++j)
+            c[j] += i * c[j - 1];
+
+    return c[maxj];
 }
 
 unsigned long b_dynamic(unsigned int n) {
-    // TODO
-    return 0;
+    unsigned long r = 0;
+    for (int i = 1; i <= n; ++i)
+        r += s_dynamic(n, i);
+
+    return r;
 }
 
 /// TESTS ///
 #include <gtest/gtest.h>
 
 TEST(TP4_Ex5, testPartitioningRecursive) {
-    EXPECT_EQ(1,s_recursive(3,3));
-    EXPECT_EQ(3025,s_recursive(9,3));
-    EXPECT_EQ(22827,s_recursive(10,6));
+    EXPECT_EQ(1, s_recursive(3, 3));
+    EXPECT_EQ(3025, s_recursive(9, 3));
+    EXPECT_EQ(22827, s_recursive(10, 6));
 
-    EXPECT_EQ(5,b_recursive(3));
-    EXPECT_EQ(203,b_recursive(6));
-    EXPECT_EQ(1382958545,b_recursive(15));
+    EXPECT_EQ(5, b_recursive(3));
+    EXPECT_EQ(203, b_recursive(6));
+    EXPECT_EQ(1382958545, b_recursive(15));
 }
 
 TEST(TP4_Ex5, testPartitioningDynamic) {
-    EXPECT_EQ(1,s_dynamic(3,3));
-    EXPECT_EQ(3025,s_dynamic(9,3));
-    EXPECT_EQ(22827,s_dynamic(10,6));
+    EXPECT_EQ(1, s_dynamic(3, 3));
+    EXPECT_EQ(3025, s_dynamic(9, 3));
+    EXPECT_EQ(22827, s_dynamic(10, 6));
 
-    EXPECT_EQ(5,b_dynamic(3));
-    EXPECT_EQ(203,b_dynamic(6));
-    EXPECT_EQ(1382958545,b_dynamic(15));
+    EXPECT_EQ(5, b_dynamic(3));
+    EXPECT_EQ(203, b_dynamic(6));
+    EXPECT_EQ(1382958545, b_dynamic(15));
 }
